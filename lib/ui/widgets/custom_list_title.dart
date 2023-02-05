1772af6138f74bca7b19d99f63136479c9e31e5e
mac <375447955552dk@gmail.com>
2023-02-05 17:28:02 +0300

    persons name

--- lib/ui/widgets/custom_list_title.dart
+++ lib/ui/widgets/custom_list_title.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:rick_and_morty/data/models/character.dart';
 
@@ -7,11 +8,49 @@ class CustomListTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(
-        results.name,
-        style: const TextStyle(color: Colors.white),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        height: MediaQuery.of(context).size.height / 7,
+        color: Color.fromRGBO(86, 86, 86, 0.8),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            CachedNetworkImage(
+              imageUrl: results.image,
+              placeholder: (context, utl) => const CircularProgressIndicator(
+                color: Colors.grey,
+              ),
+              errorWidget: (context, url, error) => const Icon(Icons.error),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width / 1.9,
+                  child: Text(
+                    results.name,
+                    overflow: TextOverflow.ellipsis,
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
 }
+
+
+//ListTile(
+ //     title: Text(
+ //       results.name,
+ //       style: const TextStyle(color: Colors.white),
+ //     ),
+ //   );
